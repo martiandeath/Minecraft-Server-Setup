7981c7b51f4cfc27acef1318391cef4211fa8471
martiandeath <69183059+martiandeath@users.noreply.github.com>
2022-12-31 17:13:39 +1100

    Update Minecraft-Server-Setup.sh

--- Minecraft-Server-Setup.sh
+++ Minecraft-Server-Setup.sh
@@ -3,9 +3,9 @@ sudo firewall-cmd --permanent --zone=public --add-port=25565/udp
 sudo firewall-cmd --reload
 sudo yum -y update && sudo yum upgrade
 sudo yum -y install jdk-19
-wget https://maven.minecraftforge.net/net/minecraftforge/forge/1.19.3-44.0.41/forge-1.19.3-44.0.41-installer.jar
-java -jar forge-1.19.3-44.0.41-installer.jar --install
-rm forge-1.19.3-44.0.41-installer.jar forge-1.19.3-44.0.41-installer.jar.log
+wget https://maven.minecraftforge.net/net/minecraftforge/forge/1.19.3-44.0.48/forge-1.19.3-44.0.48-installer.jar
+java -jar forge-1.19.3-44.0.48-installer.jar --install
+rm forge-1.19.3-44.0.48-installer.jar forge-1.19.3-44.0.48-installer.jar.log
 ./run.sh
 echo "eula=true" > eula.txt
 echo " -Xmx20G" > user_jvm_args.txt
