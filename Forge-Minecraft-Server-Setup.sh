3227af0711f7bb27b3e4c95aadff6a07cb2290cf
martiandeath <69183059+martiandeath@users.noreply.github.com>
2023-12-17 22:06:10 +1100

    Update Forge-Minecraft-Server-Setup.sh

--- Forge-Minecraft-Server-Setup.sh
+++ Forge-Minecraft-Server-Setup.sh
@@ -2,7 +2,7 @@ sudo firewall-cmd --permanent --zone=public --add-port=25565/tcp
 sudo firewall-cmd --permanent --zone=public --add-port=25565/udp
 sudo firewall-cmd --reload
 sudo yum -y update && sudo yum upgrade
-sudo yum -y install jdk-20
+sudo yum -y install jdk-21
 wget https://maven.minecraftforge.net/net/minecraftforge/forge/1.20.2-48.1.0/forge-1.20.2-48.1.0-installer.jar
 java -jar forge-1.20.2-48.1.0-installer.jar --install
 rm forge-1.20.2-48.1.0-installer.jar forge-1.20.2-48.1.0-installer.jar.log
